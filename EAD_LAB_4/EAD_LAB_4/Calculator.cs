7157e32e456e69b1b56a9a83012337441f6cb582
GaryCurran <garycurran@live.com>
2014-10-09 14:56:05 +0100

    Test class added

---
+++ EAD_LAB_4/EAD_LAB_4/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAD_LAB_4
+{
+    class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        private double a;
+        private double b;
+
+        //Write a Calculator class which contains a static method which divides 2 floating point numbers and returns the answer. 
+        //If the second number is 0 throw an ArgumentException since division by 0 is undefined. 
+        public static double DivideNumber(double a, double b)
+        {
+            double number = 0;
+            //try
+            //{
+                if (b > 0)
+                {
+                    number = a / b;
+                    Console.WriteLine("\n" + a + " / " + b + " = " + number);
+                }
+
+                else
+                {
+                    Console.WriteLine("Division of by", b);
+                    throw new DivideByZeroException();
+                }
+
+            //}
+            //catch (DivideByZeroException)
+            //{
+            //    Console.WriteLine("Division of {0} by zero.", b);
+            //}
+
+            return number;
+        }
+    }
+}
