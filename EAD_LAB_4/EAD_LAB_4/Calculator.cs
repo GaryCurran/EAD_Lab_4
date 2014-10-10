ae480b0e5662a392855f00a2e8524d455ceb3bea
GaryCurran <garycurran@live.com>
2014-10-10 11:37:39 +0100

    Execption Handling Added

--- EAD_LAB_4/EAD_LAB_4/Calculator.cs
+++ EAD_LAB_4/EAD_LAB_4/Calculator.cs
@@ -6,42 +6,27 @@ using System.Threading.Tasks;
 
 namespace EAD_LAB_4
 {
-    class Calculator
+    public class Calculator
     {
         public Calculator()
         {
 
         }
-
-        private double a;
-        private double b;
-
         //Write a Calculator class which contains a static method which divides 2 floating point numbers and returns the answer. 
         //If the second number is 0 throw an ArgumentException since division by 0 is undefined. 
         public static double DivideNumber(double a, double b)
         {
-            double number = 0;
-            //try
-            //{
-                if (b > 0)
-                {
-                    number = a / b;
-                    Console.WriteLine("\n" + a + " / " + b + " = " + number);
-                }
+          
+            if (b == 0)
+            {
+                throw new ArgumentException("Error occured: attempt to divide " +
+                    a + " by 0");
+            }
+            else
+            { 
+                return a/b;
+            }
 
-                else
-                {
-                    Console.WriteLine("Division of by", b);
-                    throw new DivideByZeroException();
-                }
-
-            //}
-            //catch (DivideByZeroException)
-            //{
-            //    Console.WriteLine("Division of {0} by zero.", b);
-            //}
-
-            return number;
         }
     }
 }
